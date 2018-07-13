6648f052684170c03e147ef6785228b287e99635
Daniel Nghiem <nghiem.daniel@yahoo.com>
2018-07-12 17:41:02 -0700

    first Commit

---
+++ Project3/src/RPSGame.java
@@ -0,0 +1,38 @@
+/*
+ * Daniel Nghiem (W99272040)
+ * CS111B
+ * Project 3:  Rock, Paper, Scissors
+ * 
+ */
+
+
+public class RPSGame() {
+
+	// Instance data
+	
+	// Number of computer wins
+	
+	// Number of user wins
+	
+	// Number of ties
+	
+	public const generateComputerPlay() {
+		// Return a randomly generated computer move
+	}
+
+	public void findWinner() {
+		// Input: user move, computer move
+		
+		// Determine the winner using the two moves and update the win count
+		
+		// Return the outcome (win/lose/tie)
+	}
+	
+	public int getCWins() {
+		return 0;
+	}
+	
+	public int getUWins() {
+		return 0;
+	}
+}
